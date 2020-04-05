2170cad7a48fe221caea8bfb00ed96e5edb31885
Luc Yriarte <luc.yriarte@thingagora.org>
2020-04-06 00:32:57 +0200

    Robot REST command: /STEPPER/{index}/{steps}

--- wifi-robot.ino
+++ wifi-robot.ino
@@ -90,14 +90,61 @@ LEDInfo ledInfos[] = {
 };
 
 
+/*
+ * STEPPER
+ */
+ 
+typedef struct {
+  int gpios[4];
+  int steps;
+} STEPPERInfo;
+
+STEPPERInfo stepperInfos[] = {
+	{
+		{12,13,14,15},
+		0
+	},
+	{
+		{0,4,5,16},
+		0
+	}
+};
+
+byte steps8[] = {
+  HIGH,  LOW,  LOW,  LOW,
+  HIGH, HIGH,  LOW,  LOW,
+   LOW, HIGH,  LOW,  LOW,
+   LOW, HIGH, HIGH,  LOW,
+   LOW,  LOW, HIGH,  LOW, 
+   LOW,  LOW, HIGH, HIGH,
+   LOW,  LOW,  LOW, HIGH,
+  HIGH,  LOW,  LOW, HIGH,
+};
+
+#define STEPPERMS 5
+void step8(int pin1, int pin2, int pin3, int pin4) {
+	int i=0;
+	while (i<32) {
+		digitalWrite(pin1, steps8[i++]);
+		digitalWrite(pin2, steps8[i++]);
+		digitalWrite(pin3, steps8[i++]);
+		digitalWrite(pin4, steps8[i++]);
+		delay(STEPPERMS);
+	}
+} 
+
+
 /* **** **** **** **** **** ****
  * Functions
  * **** **** **** **** **** ****/
 
 void setup() {
-	int i;
+	int i,j;
 	for (i=0; i < sizeof(ledInfos) / sizeof(LEDInfo); i++)
 		pinMode(ledInfos[i].gpio, OUTPUT);
+	for (i=0; i < sizeof(stepperInfos) / sizeof(STEPPERInfo); i++)
+		for (j=0; j < 4; j++)
+			pinMode(stepperInfos[i].gpios[j], OUTPUT);
 	Serial.begin(BPS_HOST);
 	wifiMacInit();
 	Serial.print("WiFi.macAddress: ");
@@ -191,6 +238,33 @@ bool handleLEDRequest(const char * req) {
 	return true;
 }
 
+void updateSTEPPERStatus(int index) {
+	while (stepperInfos[index].steps) {
+		if (stepperInfos[index].steps > 0) {
+			step8(stepperInfos[index].gpios[0],stepperInfos[index].gpios[1],stepperInfos[index].gpios[2],stepperInfos[index].gpios[3]);
+			stepperInfos[index].steps--;
+		}
+		else {
+			step8(stepperInfos[index].gpios[3],stepperInfos[index].gpios[2],stepperInfos[index].gpios[1],stepperInfos[index].gpios[0]);
+			stepperInfos[index].steps++;
+		}
+	}
+}
+
+bool handleSTEPPERRequest(const char * req) {
+	String strReq = req;
+	int index = strReq.toInt();
+	if (index < 0 || index >= sizeof(stepperInfos) / sizeof(STEPPERInfo))
+		return false;
+	stepperInfos[index].steps = strReq.substring(strReq.indexOf("/")+1).toInt();
+	updateSTEPPERStatus(index);
+	return true;
+}
+
+/*
+ * HTTP request main dispatch
+ */
+
 bool handleHttpRequest(const char * req) {
 	if (req == NULL)
 		return false;
@@ -202,6 +276,8 @@ bool handleHttpRequest(const char * req) {
 	wifiClient.println(strReq);
 	if (strReq.startsWith("LED/"))
 		return handleLEDRequest(strReq.substring(4).c_str());
+	if (strReq.startsWith("STEPPER/"))
+		return handleSTEPPERRequest(strReq.substring(8).c_str());
 	return false;
 }
 
