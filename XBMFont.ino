70d0330cc01b4bdfccad992a4bff52cb462ec643
Luc Yriarte <luc.yriarte@thingagora.org>
2021-01-02 15:17:18 +0100

    Ad-hoc mappings for charcodes > 127

--- XBMFont.ino
+++ XBMFont.ino
@@ -24,6 +24,13 @@ unsigned char * XBMFont::getBitmap(char c) {
 		return this->chars[i - 32]; // [0..95]
 	if (i >= 224 && i <= 255)
 		return this->chars[i - 128]; // [96..127]
+	// Ad-hoc mappings for charcodes > 127
+	if (i >= 192 && i <= 198) return this->chars[33]; // 'A' - 32
+	if (i >= 200 && i <= 203) return this->chars[37]; // 'E' - 32
+	if (i >= 204 && i <= 207) return this->chars[41]; // 'I' - 32
+	if (i >= 210 && i <= 214) return this->chars[47]; // 'O' - 32
+	if (i >= 217 && i <= 222) return this->chars[53]; // 'U' - 32
+	if (i == 171 || i == 187) return this->chars[02]; // '"' - 32
 	return this->chars[95]; // 127-32
 }
 
